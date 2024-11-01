93985ef7125fdae14bb6d7a18448bf417af9e40e
Herny Dai <d2567smn@163.com>
2024-11-01 12:10:49 +0800

    project init

---
+++ backend/src/main/java/com/dch/interview/mapper/PostThumbMapper.java
@@ -0,0 +1,18 @@
+package com.dch.interview.mapper;
+
+import com.dch.interview.model.entity.PostThumb;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * 帖子点赞数据库操作
+ *
+ * @author henryDai
+ * @from <a href="https://yupi.icu">编程导航知识星球</a>
+ */
+public interface PostThumbMapper extends BaseMapper<PostThumb> {
+
+}
+
+
+
+
